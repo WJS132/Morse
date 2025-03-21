4de1f13ec67af21698128cd3ca4ec233f1a8ab9b
WJS132 <johnypl326@gmail.com>
2025-03-21 15:03:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let alfabet = {
+    "a": " •- ", "ą": " •-•- ", "b": " -••• ", "c": " -•-• ", "ć": " -•-•• ",
+    "d": " -•• ", "e": " • ", "ę": " ••-•• ", "f": " ••-• ", "g": " --• ",
+    "h": " •••• ", "i": " •• ", "j": " •--- ", "k": " -•- ", "l": " •-•• ",
+    "ł": " -•-••- ", "m": " -- ", "n": " -• ", "ń": " --•-- ", "o": " --- ",
+    "ó": " ---• ", "p": " •--• ", "q": " --•- ", "r": " •-• ", "s": " ••• ",
+    "ś": " •••-••• ", "t": " - ", "u": " ••- ", "v": " •••- ", "w": " •-- ",
+    "x": " -••- ", "y": " -•-- ", "z": " --•• ", "ź": " --••- ", "ż": " --••-• ",
+    "1": " •---- ", "2": " ••--- ", "3": " •••-- ", "4": " ••••- ", "5": " ••••• ",
+    "6": " -•••• ", "7": " --••• ", "8": " ---•• ", "9": " ----• ", "0": " ----- ", " ": "/"
+};
+
+
+
+tekst = document.getElementById("tekst");
+pole = document.getElementById("pole");
+but = document.getElementById("guzik");
+
+function tlumacz() {
+    tlumaczenie = "";
+    for (const litera of tekst.value) {
+
+        tlumaczenie += alfabet[litera];
+
+    }
+    pole.textContent = tlumaczenie;
+
+}
+but.addEventListener("click", tlumacz);
+
+
+
+
+
+
